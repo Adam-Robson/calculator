9ac5d8d7483f852fcef00138ea246dad1d5dcd35
Adam Robson <101651926+Adam-Robson@users.noreply.github.com>
2022-06-23 19:58:50 -0700

    'draft + - * / capability to calculator'

--- app.js
+++ app.js
@@ -1,8 +1,59 @@
 // import needed modules
+import { add } from '../utils.js';
+
 
 // define and grab DOM elements
+const addInput1 = document.getElementById('add-input-1');
+const addInput2 = document.getElementById('add-input-2');
+const addButton = document.getElementById('add-button');
+const addResult = document.getElementById('add-result');
 
+addButton.addEventListener('click', () => {
+    const num1 = Number(addInput1.value);
+    const num2 = Number(addInput2.value);
+    addResult.textContent = add(num1, num2);
+});
 // event handlers - what needs to happen?
-    // logic and calculations
-    // update UI
+ // logic and calculations
+     // update UI
+
+import { sub } from '../utils.js';
+
+const subInput1 = document.getElementById('sub-input-1');
+const subInput2 = document.getElementById('sub-input-2');
+const subButton = document.getElementById('sub-button');
+const subResult = document.getElementById('sub-result');
+
+subButton.addEventListener('click', () => {
+    const num1 = Number(subInput1.value);
+    const num2 = Number(subInput2.value);
+    subResult.textContent = sub(num1, num2);
+});  
+
+import { multiply } from '../utils.js';
+
+const multiplyInput1 = document.getElementById('multiply-input-1');
+const multiplyInput2 = document.getElementById('multiply-input-2');
+const multiplyButton = document.getElementById('multiply-button');
+const multiplyResult = document.getElementById('multiply-result');
+
+multiplyButton.addEventListener('click', () => {
+    const num1 = Number(multiplyInput1.value);
+    const num2 = Number(multiplyInput2.value);
+    multiplyResult.textContent = multiply(num1, num2);
+});  
+
+import { divide } from '../utils.js';
+
+const divideInput1 = document.getElementById('divide-input-1');
+const divideInput2 = document.getElementById('divide-input-2');
+const divideButton = document.getElementById('divide-button');
+const divideResult = document.getElementById('divide-result');
+
+divideButton.addEventListener('click', () => {
+    const num1 = Number(divideInput1.value);
+    const num2 = Number(divideInput2.value);
+    divideResult.textContent = divide(num1, num2);
+});  
+
 
--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,43 @@
 // IMPORT code to test here
-// import { add } from '../utils.js';
-
+import { add } from '../utils.js';
 const test = QUnit.test;
-
-test('test your pure functions...', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
-
+test('adding numbers', (expect) => {
+    //Arrange, set up your arguments and expectations
+    const num1 = 3;
+    const num2 = 4;
+    const expected = 7;
+    const actual = add(num1, num2);
     //Expect
     // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+import { sub } from '../utils.js';
+const tezt = QUnit.test;
+tezt('subtracting numbers', (expect) => {
+    const num1 = 5;
+    const num2 = 1;
+    const expected = 4;
+    const actual = sub(num1, num2);
+    expect.equal(actual, expected);
+});
+
+import { multiply } from '../utils.js';
+const text = QUnit.test;
+text('multiply numbers', (expect) => {
+    const num1 = 9;
+    const num2 = 7;
+    const expected = 63;
+    const actual = multiply(num1, num2);
+    expect.equal(actual, expected);
+});
+
+import { divide } from '../utils.js';
+const teet = QUnit.test;
+teet('divide numbers', (expect) => {
+    const num1 = 12;
+    const num2 = 6;
+    const expected = 2;
+    const actual = divide(num1, num2);
     expect.equal(actual, expected);
 });
